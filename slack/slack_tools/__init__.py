69fdd43ba36b15f481707bd36a17daea1ed0eebd
michagonzo77 <caleb.g@nylas.com>
2024-09-25 11:29:05 -0700

    fixes

--- slack/slack_tools/__init__.py
+++ slack/slack_tools/__init__.py
@@ -1,27 +1,3 @@
-from .tools.operations import (
-    slack_send_message,
-    slack_upload_file,
-    slack_list_channels,
-    slack_create_channel,
-    slack_invite_user,
-    slack_get_channel_history,
-    slack_update_message,
-    slack_delete_message,
-    slack_add_reaction,
-    slack_remove_reaction,
-    slack_search_messages,
-)
+# slack_tools/__init__.py
 
-__all__ = [
-    'slack_send_message',
-    'slack_upload_file',
-    'slack_list_channels',
-    'slack_create_channel',
-    'slack_invite_user',
-    'slack_get_channel_history',
-    'slack_update_message',
-    'slack_delete_message',
-    'slack_add_reaction',
-    'slack_remove_reaction',
-    'slack_search_messages',
-]
+from .tools import *
