0daa09b6645aae519866ad755553d941f2256ea2
michagonzo77 <caleb.g@nylas.com>
2024-09-25 11:33:37 -0700

    fixes

--- slack/slack_tools/tools/operations.py
+++ slack/slack_tools/tools/operations.py
@@ -344,7 +344,7 @@ except SlackApiError as e:
 
 
 # Register all Slack tools
-for tool in [slack_send_message, slack_upload_file, slack_list_channels, slack_create_channel, 
+for tool in [slack_send_message, slack_upload_file, slack_list_channels, 
              slack_invite_user, slack_get_channel_history, slack_update_message, slack_delete_message, 
              slack_add_reaction, slack_remove_reaction, slack_search_messages, slack_send_message_with_image]:
     tool_registry.register("slack", tool)
