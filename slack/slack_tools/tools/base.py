a72379e9bf89865de1a3e32ca72eb428fbc6630c
michagonzo77 <caleb.g@nylas.com>
2024-09-25 12:48:33 -0700

    fixes

--- slack/slack_tools/tools/base.py
+++ slack/slack_tools/tools/base.py
@@ -4,9 +4,7 @@ SLACK_ICON_URL = "https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_
 
 class SlackTool(Tool):
     def __init__(self, name, description, content, args, long_running=False, thread_context=False, mermaid_diagram=None):
-        env = ["SLACK_API_KEY"]
-        if thread_context:
-            env.extend(["SLACK_THREAD_TS", "SLACK_CHANNEL_ID"])
+        env = ["SLACK_API_KEY", "SLACK_THREAD_TS", "SLACK_CHANNEL_ID"]
 
         super().__init__(
             name=name,
