127d57867a4d7904b4d4b78157124f30c7ebc1af
michagonzo77 <caleb.g@nylas.com>
2024-09-26 10:55:29 -0700

    fixes

--- freshworks/setup.py
+++ freshworks/setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kubiya-slack-tools",
+    name="kubiya-freshworks-tools",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "kubiya-sdk",
-        "slack_sdk"
+        "kubiya-sdk"
     ],
 )
