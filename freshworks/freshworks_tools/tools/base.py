1f6838ab8348ae2773c537e4a96bd4db204945f6
michagonzo77 <caleb.g@nylas.com>
2024-09-26 10:01:42 -0700

    fixes

--- freshworks/freshworks_tools/tools/base.py
+++ freshworks/freshworks_tools/tools/base.py
@@ -4,19 +4,17 @@ SLACK_ICON_URL = "https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_
 
 class FreshworksTool(Tool):
     def __init__(self, name, description, content, args, long_running=False, thread_context=False, mermaid_diagram=None):
-        env = ["SLACK_API_KEY"]
-        if thread_context:
-            env.extend(["SLACK_THREAD_TS", "SLACK_CHANNEL_ID"])
+        env = ["SLACK_API_KEY", "SLACK_THREAD_TS", "SLACK_CHANNEL_ID", "GRAFANA_API_KEY", "TOOLS_GH_TOKEN"]
 
         super().__init__(
             name=name,
             description=description,
             icon_url=SLACK_ICON_URL,
-            type="python",
+            type="docker",
+            image="python:3.12",
             content=content,
             args=args,
             env=env,
             long_running=long_running,
-            requirements=["slack_sdk", "requests"],
             mermaid_diagram=mermaid_diagram
         )
