d2a8b5a6edd273d163e63f3b94fccb5fc465e5cf
michagonzo77 <caleb.g@nylas.com>
2024-09-26 21:57:24 -0700

    fixes

--- freshworks/freshworks_tools/tools/send_image.py
+++ freshworks/freshworks_tools/tools/send_image.py
@@ -9,7 +9,6 @@ get_grafana_render_url = FreshworksTool(
     content="""python -c '
 import os
 from urllib.parse import urlparse
-import argparse
 
 def generate_grafana_render_url(grafana_dashboard_url):
     # Parse the Grafana dashboard URL to extract UID and slug
@@ -28,12 +27,8 @@ def generate_grafana_render_url(grafana_dashboard_url):
     render_url = f"{parsed_url.scheme}://{parsed_url.netloc}/render/d/{dashboard_uid}/{dashboard_slug}"
     return render_url
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate the render URL for a Grafana dashboard")
-    parser.add_argument("grafana_dashboard_url", type=str, help="URL of the Grafana dashboard to generate the render URL for")
-    args = parser.parse_args()
-    render_url = generate_grafana_render_url(os.environ["grafana_dashboard_url"])
-    print(render_url)
+render_url = generate_grafana_render_url(grafana_dashboard_url)
+print(render_url)
 '""",
     args=[
         Arg(
